55dda1b179f5c7caf324b92848723ed89de59c6a
elijah <ex@gmail.com>
2020-06-19 17:15:14 +0300

    компонент лого

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -5,6 +5,7 @@ const Logo = () => (
     <div className="Logo-wrapper">
         <div className="Circle"></div>
         <div className="Rectangle"></div>
+        <div className="Circle"></div>
         <div className="Rectangle-inclined"></div>
     </div>
 )
