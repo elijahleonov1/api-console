7991b2864addf06744e446064cac506fba62ab69
elijah <ex@gmail.com>
2020-06-19 22:51:34 +0300

    форма аутификации

---
+++ src/containers/Authentication/Authentication.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const Authentication = () => {
+    return <div>authentication</div>
+}
+
+export default connect(null, null)(Authentication)
