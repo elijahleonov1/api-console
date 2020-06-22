62b9b157b7e9007656545499f399e0b6c607ca7e
elijah <ex@gmail.com>
2020-06-23 02:32:54 +0300

    добавил обертку для более удобной работы с Sendsay

--- src/utils/api/sendsay.js
+++ src/utils/api/sendsay.js
@@ -1,3 +1,45 @@
 import Sendsay from 'sendsay-api'
 
-export default new Sendsay()
+class SendsayDecorator {
+    constructor() {
+        this.LOCAL_STOREGE_NAME = 'USER_AUTH_DATA'
+        this._sendsay = null
+    }
+
+    async login({ login = '', sublogin = '', password = '' }) {
+        const sendsay = new Sendsay()
+        return await sendsay
+            .login({ login, sublogin, password })
+            .then(() => {
+                this._newSendsayInstance({ login, sublogin, password })
+                return {
+                    isAuth: true,
+                    errorMessage: null,
+                }
+            })
+            .catch(({ id, explain }) => ({
+                isAuth: false,
+                errorMessage: JSON.stringify({ id, explain }),
+            }))
+    }
+
+    _newSendsayInstance({ login = '', sublogin = '', password = '' }) {
+        this._sendsay = new Sendsay({
+            auth: {
+                login,
+                sublogin,
+                password,
+            },
+        })
+        return this._sendsay
+    }
+
+    async request(req) {
+        return await this._sendsay
+            .request(req)
+            .then((data) => data)
+            .catch((e) => e)
+    }
+}
+
+export default new SendsayDecorator()
